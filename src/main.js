de676b2d451d1de3a662698be9fcaa753d7870e5
Roiabr <roiabr15@gmail.com>
2021-11-17 02:00:13 +0200

    removing comments

--- src/main.js
+++ src/main.js
@@ -27,7 +27,7 @@ const nodemailer = require("nodemailer");
     secure: true, // true for 465, false for other ports
     auth: {
       user: process.env.MAIL_USER_EMAIL,
-      pass: process.env.MAIL_USER_PASSWORD,
+      pass: process.env.MAIL_USER_PASSWORDs,
     },
   });
 
@@ -44,23 +44,3 @@ const nodemailer = require("nodemailer");
     `,
   });
 })();
-
-// // async..await is not allowed in global scope, must use a wrapper
-// async function main() {
-//   // Generate test SMTP service account from ethereal.email
-//   // Only needed if you don't have a real mail account for testing
-//   let testAccount = await nodemailer.createTestAccount();
-
-//   // create reusable transporter object using the default SMTP transport
-
-//   // send mail with defined transport object
-
-//   console.log("Message sent: %s", info.messageId);
-//   // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-//   // Preview only available when sending through an Ethereal account
-//   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-//   // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-// }
-
-// main().catch(console.error);
